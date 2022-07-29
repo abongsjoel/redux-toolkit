37007c62db64f331f57d5740263a255c6495f81f
Chi Joel <abongsjoel@gmail.com>
2022-07-29 09:40:04 +0100

    Add starter folder

---
+++ starter/src/App.js
@@ -0,0 +1,12 @@
+import Stories from "./components/Stories";
+function App() {
+  return (
+    <div>
+      <h1>Hello and Welcome</h1>
+      <h3>My Favorite Stories on Medium Are:</h3>
+      <Stories />
+    </div>
+  );
+}
+
+export default App;
---
+++ starter/src/components/Stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Stories = () => {
+  return (
+    <ul>
+      <li>Redux — A birds-eye view</li>
+      <li>React Context and the UseContext hook</li>
+      <li>TypeScript — Difference between any and unknown Types</li>
+    </ul>
+  );
+};
+
+export default Stories;
